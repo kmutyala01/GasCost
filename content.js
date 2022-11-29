36bdd185abf39d27a4341489cf8f758990b1a6e1
kmutyala01 <karthiksatya@gmail.com>
2022-11-29 16:39:10 -0600

    Temporary fix to reloading issue, added match only for google maps.

--- content.js
+++ content.js
@@ -1,9 +1,8 @@
 const mpg = 35.0
 const costOfGallon = 2.70
 
-const collection = document.getElementsByClassName("XdKEzd");
+alert("Match!")
 
-console.log(collection)
 
 const calculateCost = (dist) =>{
     
@@ -12,24 +11,48 @@ const calculateCost = (dist) =>{
 }
 
 
+const generate = (collection) => {
+    
 
-for (var i = 0; i < collection.length; i++) {
-    
-    //parsing distance for trip
-    let dist = collection[i].outerText.split('\n');
-    let end = dist[1].indexOf(' ');
-    dist = dist[1].substring(0, end);
-    dist = parseFloat(dist.replace(/,/g, ''));
-    
-    //Calculating the cost
-    let cost = calculateCost(dist);
+            console.log(collection.length)
+            for (var i = 0; i < collection.length; i++) {
+                
 
-    //Adding the cost to DOM
-    const div = document.createElement("div")
-    div.innerText = "$" + cost;
-    collection[i].appendChild(div)
-    
-    
-    
+                //parsing distance for trip
+                let dist = collection[i].outerText.split('\n');
+                let end = dist[1].indexOf(' ');
+                dist = dist[1].substring(0, end);
+                dist = parseFloat(dist.replace(/,/g, ''));
+                
+                //Calculating the cost
+                let cost = calculateCost(dist);
+                console.log(cost);
+            
+                //Adding the cost to DOM
+                const div = document.createElement("div")
+                div.innerText = "$" + cost;
+                collection[i].appendChild(div)
+
+            }
 
 }
+
+
+
+
+window.addEventListener("keyup", (event) => {
+    
+    if (event.key === "Enter") {
+        
+        setTimeout( () => {
+            collection = document.getElementsByClassName("XdKEzd");
+            generate(collection);
+    
+        },3000)
+        
+    
+        
+        
+        
+    }
+});
